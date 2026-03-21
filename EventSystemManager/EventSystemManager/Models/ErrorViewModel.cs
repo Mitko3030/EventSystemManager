b3a3b8af6f678718d318c7f43cb6abf05561f31e
Mitko3030 <velichkovdimitar2008@gmail.com>
2026-03-21 14:33:23 +0200

    Created the base

--- EventSystemManager/EventSystemManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EventSystemManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
