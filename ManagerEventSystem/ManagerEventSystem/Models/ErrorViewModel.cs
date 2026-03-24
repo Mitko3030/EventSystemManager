2074f9bc1398254648665cb301b38545ec324507
Mitko3030 <velichkovdimitar2008@gmail.com>
2026-03-24 18:38:34 +0200

    sth

---
+++ ManagerEventSystem/ManagerEventSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagerEventSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
